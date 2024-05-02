f2ac3a162dc570f18c7cf2bfa43a106104095d48
lukepadiachy <148848073+lukepadiachy@users.noreply.github.com>
2024-05-02 13:41:50 +0200

    first commit

---
+++ IgadiYamiDesign/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using IgadiYamiDesign.Pages;
+
+namespace IgadiYamiDesign
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new StartPage();
+        }
+    }
+}
---
+++ IgadiYamiDesign/Pages/StartPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace IgadiYamiDesign.Pages;
+
+public partial class StartPage : ContentPage
+{
+	public StartPage()
+	{
+		InitializeComponent();
+	}
+}
