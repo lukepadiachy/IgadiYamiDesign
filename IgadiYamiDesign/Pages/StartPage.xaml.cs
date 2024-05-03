f3b471dcb5841c784d53b34b42cd1f59ffffdc1d
lukepadiachy <148848073+lukepadiachy@users.noreply.github.com>
2024-05-03 09:32:58 +0200

    Ui Changes

--- IgadiYamiDesign/Pages/StartPage.xaml.cs
+++ IgadiYamiDesign/Pages/StartPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class StartPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void GetStarted_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushModalAsync(new SignupPage());
+    }
 }
