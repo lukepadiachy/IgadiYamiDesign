3fb852acdf779176876779faad230dc2314623c7
Simbonile03 <simbonilematshaya03@gmail.com>
2024-05-07 10:41:01 +0200

    added font

--- IgadiYamiDesign/MauiProgram.cs
+++ IgadiYamiDesign/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace IgadiYamiDesign
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("montserrat.ttf", "mont"); 
+
                 });
 
 #if DEBUG
