392026c0fea8e4beb428b816b2a422f4e64783a3
Simbonile03 <simbonilematshaya03@gmail.com>
2024-05-21 11:38:50 +0200

    njb

--- IgadiYamiDesign/App.xaml.cs
+++ IgadiYamiDesign/App.xaml.cs
@@ -6,20 +6,23 @@ namespace IgadiYamiDesign
     {
         public App()
         {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+>>>>>>> Stashed changes
             InitializeComponent();
            // MainPage = new StartPage();
             // Set the theme based on system settings
-            App.Current.UserAppTheme = App.Current.RequestedTheme;
+           // App.Current.UserAppTheme = App.Current.RequestedTheme;
 
-            MainPage = new CreatAccount();
-=======
+            MainPage = new StartPage();
              InitializeComponent();
-            MainPage = new SettingsPage();
+            //MainPage = new SettingsPage();
             // Set the theme based on system settings
-            App.Current.UserAppTheme = App.Current.RequestedTheme;
+           // App.Current.UserAppTheme = App.Current.RequestedTheme;
 
+<<<<<<< Updated upstream
             //MainPage = new StartPage();
 >>>>>>> c1d00ce92c043daf40563a905076f87703029851
            // MainPage = new NavigationPage(new AboutUs());
@@ -29,6 +32,9 @@ namespace IgadiYamiDesign
             // Set the theme based on system settings
             App.Current.UserAppTheme = App.Current.RequestedTheme;
 >>>>>>> 6950658b586fa8a4f8d312810841ba4bc1a3de04
+=======
+           
+>>>>>>> Stashed changes
         }
     }
 }
